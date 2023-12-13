9e89efd0a0c9fbc4f61126e6dbc3b35f74c2bb91
Youngnoh <61908151+comejtome508@users.noreply.github.com>
2023-12-13 18:43:55 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Youngnoh",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "youngnoh.goh@gmail.com",
+    linkedin: "youngnohgoh",
+    github: "comejtome508",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `Youngnoh Goh`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Youngnoh Goh",
+    description: "My name is Youngnoh Goh!",
   },
 
   // CONFIG configration (required)
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
